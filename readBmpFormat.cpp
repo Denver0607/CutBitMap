192bb32844172b3a1630b750e8df700b65b43ab7
Duy Nguyen <nhdduy67@gmail.com>
2023-03-18 00:28:55 +0700

    Add fstream library into readBmpFormat.h

---
+++ readBmpFormat.cpp
@@ -0,0 +1,7 @@
+#include"readBmpFormat.h"
+
+bool isBMP(string fi)
+{
+    ifstream input(fi,ios::binary);
+    
+}
--- readBmpFormat.h
+++ readBmpFormat.h
@@ -1,4 +1,5 @@
 #include<stdint.h>
+#include<fstream>
 #include<stdio.h>
 #include<string>
 
